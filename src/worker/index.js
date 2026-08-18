5ca1507b8ecdb567054326716271e0cbac551378
Mark Maetschke <mark.maetschke@urgentpublish.de>
2026-08-18 07:50:51 +0000

    Icons: Motiv verkleinert, Rest vollflaechig mit Icon-Farbe gefuellt (kein grauer Innenrand)

--- src/worker/index.js
+++ src/worker/index.js
@@ -4,7 +4,7 @@ import { handleWx } from './wx.js';
 import { handleElev } from './elev.js';
 import { handleDgm } from './dgm.js';
 import { handleCourses } from './courses.js';
-const FP_BUILD = '2026-08-18 · 09:50 · parallel+icons4';
+const FP_BUILD = '2026-08-18 · 09:58 · icons5';
 import { handleHoles } from './holes.js';
 import { handleResearch } from './research.js';
 import { handleAccountDelete } from './account.js';
